abf69c889bf38a4b86be0e6ba3034007bb0491ef
niccolo99mandelli <n.mandelli2@campus.unimib.it>
2023-09-02 01:27:27 +0200

    Fix delete-event in dialog. Update NewProject and LoadProject

--- src/SOPHYSM_app.jl
+++ src/SOPHYSM_app.jl
@@ -15,6 +15,7 @@ thresholdDialog = SOPHYSM_app["thresholdDialog"]
 newProjectDialog = SOPHYSM_app["newProjectDialog"]
 projectAlreadyExistMessage = SOPHYSM_app["projectAlreadyExistMessage"]
 invalidNameProjectMessage = SOPHYSM_app["invalidNameProjectMessage"]
+loadProjectDialog = SOPHYSM_app["loadProjectDialog"]
 ## menuBar
 newProjectButton = SOPHYSM_app["newProjectButton"]
 loadProjectButton = SOPHYSM_app["loadProjectButton"]
@@ -36,51 +37,32 @@ newProjectOkButton = SOPHYSM_app["newProjectOkButton"]
 newProjectCancelButton = SOPHYSM_app["newProjectCancelButton"]
 projectNameLabel = SOPHYSM_app["projectNameLabel"]
 projectNameEntry = SOPHYSM_app["projectNameEntry"]
-## errorNameMessage
+# errorNameMessage
 errorNameCancelButton = SOPHYSM_app["errorNameCancelButton"]
-## invalidNameMessage
+# invalidNameMessage
 invalidNameProjectButton = SOPHYSM_app["invalidNameProjectButton"]
+## loadProjectDialog
+loadProjectOkButton = SOPHYSM_app["loadProjectOkButton"]
+loadProjectCancelButton = SOPHYSM_app["loadProjectCancelButton"]
 
 ## Button Listener
 signal_connect(segmentationButton, "button-press-event") do widget, event
-    showall(thresholdDialog)
+    run(thresholdDialog)
 end
 
-id_new_project = signal_connect(newProjectButton, "button-press-event") do widget, event
-    showall(newProjectDialog)
+## menuBar Buttons
+signal_connect(newProjectButton, "button-press-event") do widget, event
+    run(newProjectDialog)
 end
 
-# newProjectDialog element
-id_new_project_1 = signal_connect(newProjectOkButton, "button-press-event") do widget, event
-    filename = get_gtk_property(projectNameEntry, :text, String)
-    filepath_directory = joinpath(@__DIR__, "..", "workspace", filename)
-    if filename == ""
-        showall(invalidNameProjectMessage)
-    else
-        if isdir(filepath_directory)
-            showall(projectAlreadyExistMessage)
-        else
-            # Build Project Folder
-            mkdir(filepath_directory)
-        end
-    end
+signal_connect(loadProjectButton, "button-press-event") do widget, event
+    run(loadProjectDialog)
 end
 
-signal_connect(newProjectCancelButton, "button-press-event") do widget, event
-    destroy(newProjectDialog)
+signal_connect(closeProjectButton, "button-press-event") do widget, event
+    ##
 end
 
-# errorNameMessage element
-signal_connect(errorNameCancelButton, "button-press-event") do widget, event
-    destroy(projectAlreadyExistMessage)
-end
-
-# invalidNameMessage element
-signal_connect(invalidNameProjectButton, "button-press-event") do widget, event
-    destroy(invalidNameProjectMessage)
-end
-
-# quitButton
 signal_connect(quitButton, "button-press-event") do widget, event
     destroy(mainWindow)
     destroy(thresholdDialog)
@@ -89,7 +71,61 @@ signal_connect(quitButton, "button-press-event") do widget, event
     destroy(invalidNameProjectMessage)
 end
 
+signal_connect(loadImageButton, "button-press-event") do widget, event
+    ##
+end
+
+## newProjectDialog elements
+signal_connect(newProjectDialog, "delete-event") do widget, event
+    hide(newProjectDialog)
+end
+
+signal_connect(newProjectOkButton, "button-press-event") do widget, event
+    filename = get_gtk_property(projectNameEntry, :text, String)
+    filepath_directory = joinpath(@__DIR__, "..", "workspace", filename)
+    if filename == ""
+        run(invalidNameProjectMessage)
+    else
+        if isdir(filepath_directory)
+            run(projectAlreadyExistMessage)
+        else
+            # Build Project Folder
+            mkdir(filepath_directory)
+            hide(newProjectDialog)
+            set_gtk_property!(descriptionLabel, :label, "Project Name : "*filename)
+            set_gtk_property!(loadButton, :sensitive, true)
+            set_gtk_property!(loadImageButton, :sensitive, true)
+        end
+    end
+end
+
+signal_connect(newProjectCancelButton, "button-press-event") do widget, event
+    hide(newProjectDialog)
+end
+# errorNameMessage element
+signal_connect(errorNameCancelButton, "button-press-event") do widget, event
+    hide(projectAlreadyExistMessage)
+end
+# invalidNameMessage element
+signal_connect(invalidNameProjectButton, "button-press-event") do widget, event
+    hide(invalidNameProjectMessage)
+end
+
+## loadProjectDialog elements
+signal_connect(loadProjectDialog, "delete-event") do widget, event
+    hide(loadProjectDialog)
+end
+
+signal_connect(loadProjectOkButton, "button-press-event") do widget, event
+    get_gtk_property(loadProjectDialog, :label, String)
+    hide(loadProjectDialog)
+end
+
+signal_connect(loadProjectCancelButton, "button-press-event") do widget, event
+    hide(loadProjectDialog)
+end
+
+
 showall(mainWindow)
 
-## FIX SECOND OPENING DIALOG
 ### end of file -- SOPHYSM_app.jl
