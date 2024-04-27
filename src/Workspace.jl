063815d3a04a7cddbc9fddeb3d14dcd9e6a7b0fe
Gabriele Moisè <g.moise@campus.unimib.it>
2024-04-27 13:12:02 +0200

    Updated Workspace logic.

--- src/Workspace.jl
+++ src/Workspace.jl
@@ -1,42 +1,59 @@
-### Includes the logic to save and update the current workspace
-### the user is using.
+### Includes the logic to save and update the current user workspace
 module Workspace
 
 ### Packages
 using JSON
 
-
 ### Exported functions
 export set_workspace_dir, get_workspace_dir
 
-# Function to get the default documents folder based on the operating system
+### Exported constants
+export CONFIG_DIR, DATA_DIR
+
+# identify the standard directories for data and resources based on the operating system
 if Sys.islinux()
-    # according to XDG Standards: $XDR_CONFIG_HOME is $HOME/.config
-    config_dir = joinpath(homedir(), ".config", "SOPHYSM")
+    # according to XDG Standards: $XDR_CONFIG_HOME
+    const CONFIG_DIR = joinpath(homedir(), ".config", "SOPHYSM")
+    # according to XDG Standards: $XDR_DATA_HOME is $HOME/.local/share
+    # Initialize the standard Workspace directory if none has been set
+    const DATA_DIR = joinpath(homedir(), ".local", "share", "SOPHYSM")
 
 elseif Sys.iswindows()
-    # %APPDATA%
-    config_dir = joinpath(homedir(), "AppData", "Roaming", "SOPHYSM")
-
+    # Config data will be saved in %APPDATA% into Local folder
+    const CONFIG_DIR = joinpath(homedir(), "AppData", "Local", "SOPHYSM")
+    const DATA_DIR = CONFIG_DIR
+    
 elseif Sys.isapple()
-    config_dir = joinpath(homedir(), "Library/.../SOPHYSM")
-# For all the other Operating systems:
+    # Preferences is macOS equivalent for Linux .config folder
+    const CONFIG_DIR = joinpath(homedir(), "Library", "Application Support", "BIMIB-DISCo.SOPHYSM")
+    const DATA_DIR = CONFIG_DIR
 
+# For all the other operating systems the configuration files will be saved in
+# SOPHYSM.jl folder
 else 
-    config_dir = @__DIR__
+    const CONFIG_DIR = @__DIR__
+    const DATA_DIR = joinpath(homedir(), "SOPHYSM")
 end
 
-function default_documents_folder()
-    return joinpath(homedir(), "Documents")
+# initialize the standard directories on first launch of the app on the system
+if !isdir(CONFIG_DIR) 
+    mkdir(CONFIG_DIR)
 end
 
+if !isdir(DATA_DIR) 
+    mkdir(DATA_DIR)
+end
+
+function default_workspace_folder()
+    return joinpath(DATA_DIR, "SOPHYSM-Workspace")
+end
 
 function set_workspace_dir(new_workspace_dir::AbstractString)
-    settings_file = joinpath(config_dir, "SOPHYSM_settings.json")
+    settings_file = joinpath(CONFIG_DIR, "SOPHYSM_settings.json")
     settings = Dict("workspace_dir" => new_workspace_dir)
 
+    # create settings_file if doesn't exist
     if !isfile(settings_file)
-        mkdir(config_dir)
         touch(settings_file)
     end
 
@@ -47,8 +64,8 @@ end
 
 # Function to load or set the workspace directory
 function get_workspace_dir()
-    settings_file = joinpath(config_dir, "SOPHYSM_settings.json")
-    default_workspace_dir = default_documents_folder()
+    settings_file = joinpath(CONFIG_DIR, "SOPHYSM_settings.json")
+    default_workspace_dir = default_workspace_folder() 
 
     # If the settings file exists, load the workspace directory from there
     if isfile(settings_file)
