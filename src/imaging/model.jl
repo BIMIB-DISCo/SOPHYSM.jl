21710c4be8d30d1d6619d820ed26449b53a0de92
Riccardo Pretali <r.pretali@campus.unimib.it>
2024-05-06 16:42:48 +0200

    Implemented upsampling path.

--- src/imaging/model.jl
+++ src/imaging/model.jl
@@ -11,8 +11,21 @@ function conv_3x3(in_ch::Int, out_ch::Int)
 end
 
 # Crop and Concatenate the feature map
-function copy_and_crop(x, contracting_x)
-    # TODO
+function copy_and_crop(x, bridge)
+    # Calculate the difference in dimensions
+    dx = size(bridge, 1) - size(x, 1)
+    dy = size(bridge, 2) - size(x, 2)
+
+    # Ensure that dx and dy are non-negative
+    if dx < 0 || dy < 0
+        error("The dimension of the bridge feature map is smaller than that of x, cropping not feasible.")
+    end
+
+    # Perform centered cropping of bridge
+    cropped_bridge = @views bridge[div(dx,2) + 1 : end - div(dx,2), div(dy,2) + 1 : end - div(dy,2), :, :]
+
+    # Concatenate along the channel axis (third dimension)
+    return cat(x, cropped_bridge, dims = 3)
 end
 
 # A 2x2 max pooling operation with stride 2
@@ -20,9 +33,9 @@ function max_pool_2x2()
     MaxPool((2, 2), stride = 2)
 end
 
-# A 2x2 convolutional layer
+# A 2x2 up-convolutional layer
 function up_conv_2x2(in_ch::Int, out_ch::Int)
-    Conv((2, 2), in_ch => out_ch)
+    ConvTranspose((2, 2), in_ch => out_ch)
 end
 
 # A 1x1 convolutional layer
@@ -53,7 +66,10 @@ function build_unet()
     )
 
     upsampling = Chain(
-        # TODO
+       up_conv_2x2(1024, 512),
+       up_conv_2x2(512, 256),
+       up_conv_2x2(256, 128),
+       up_conv_2x2(128, 64)
     )
 
     unet(downsampling, upsampling)
