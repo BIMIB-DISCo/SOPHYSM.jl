556ce7993d2e463b93003c5101e21092b4907231
Riccardo Pretali <r.pretali@campus.unimib.it>
2024-09-17 19:33:01 +0200

    Updated model.

--- src/imaging/model.jl
+++ src/imaging/model.jl
@@ -1,16 +1,16 @@
 """
     UNet
 
-    Defines the complete U-Net architecture comprising separate components for
-    different phases of the network.
+Defines the complete U-Net architecture comprising separate components for
+different phases of the network:
 
-    - The downsampling path reduces spatial dimensions and increases feature
-      channels.
-    - The bottleneck is the deepest part where feature abstraction is maximum.
-    - The upsampling path increases spatial dimensions and merges features from
-      the downsampling path.
-    - The final output layer maps the deep features to the desired number of
-      output classes or channels.
+- Downsampling path: Reduces spatial dimensions and increases feature
+  channels.
+- Bottleneck: The deepest part where feature abstraction is maximum.
+- Upsampling path: Increases spatial dimensions and merges features from
+  the downsampling path.
+- Output layer: Maps the deep features to the desired number of output
+  classes or channels.
 """
 struct UNet
     downsample::Chain
@@ -22,12 +22,12 @@ end
 """
     UNetDownBlock
 
-    Represents a single block in the downsampling path containing convolutional
-    and pooling layers.
+Represents a single block in the downsampling path containing convolutional
+and pooling layers.
 
-    - Sequential convolutional layers for feature extraction.
-    - Max pooling layer to reduce spatial dimensions by taking the maximum
-      value in a local neighborhood.
+- Applies sequential convolutional layers with activation and batch
+  normalization.
+- Uses max pooling to reduce spatial dimensions.
 """
 struct UNetDownBlock
     conv::Chain
@@ -35,14 +35,12 @@ struct UNetDownBlock
 end
 
 """
-    UNetBottleneck
+    UNetBottleneckBlock
 
-    Represents the bottleneck block in the U-Net architecture.
+Represents the bottleneck block in the U-Net architecture.
 
-    - Applies convolutional layers.
-    - Focuses on maximum feature abstraction at the deepest point of the
-      network.
-    - Bridges the downsampling and upsampling paths.
+- Applies convolutional layers with activation and batch normalization.
+- Focuses on maximum feature abstraction at the deepest point of the network.
 """
 struct UNetBottleneckBlock
     conv::Chain
@@ -51,11 +49,12 @@ end
 """
     UNetUpBlock
 
-    Represents a block in the upsampling path that uses transposed convolutions
-    to increase spatial dimensions.
+Represents a block in the upsampling path that uses transposed convolutions
+to increase spatial dimensions.
 
-    - Transposed convolutional layer to upscale the feature map.
-    - Regular convolutional layers to refine features after upsampling.
+- Uses transposed convolutional layers to upscale the feature map.
+- Applies regular convolutional layers with activation and batch normalization
+  after upsampling.
 """
 struct UNetUpBlock
     upconv::Chain
@@ -65,10 +64,10 @@ end
 """
     UNetOutputBlock
 
-    Represents the output block of the U-Net.
-    
-    - A 1x1 convolution to map the deep feature maps to the desired number of
-      output channels.
+Represents the output block of the U-Net.
+
+- A 1x1 convolution to map the deep feature maps to the desired number of
+  output channels.
 """
 struct UNetOutputBlock
     conv::Chain
@@ -77,12 +76,12 @@ end
 """
     kaiming_init(out_chs, in_chs, filter)
 
-    Initializes the weights for convolutional layers using the Kaiming
-    initialization method.
+Initializes the weights for convolutional layers using the Kaiming
+initialization method.
 
-    - This method sets the weights based on a normal distribution with a
-      standard deviation of sqrt(2 / number of input channels), which helps
-      maintain the variance of activations through the layers.
+- Sets the weights based on a normal distribution with a standard deviation
+  of sqrt(2 / number of input channels), which helps maintain the variance
+  of activations through the layers.
 """
 function kaiming_init(out_chs, in_chs, filter)
     std_dev = sqrt(2 / in_chs)
@@ -93,11 +92,11 @@ end
 """
     conv_3x3(in_chs::Int, out_chs::Int)
 
-    Constructs two consecutive 3x3 convolutional layers without padding,
-    reducing the spatial size by 2 pixels.
+Constructs two consecutive 3x3 convolutional layers with ReLU activation
+and batch normalization.
 
-    - First convolution reduces dimension and applies ReLU.
-    - Second convolution further processes the feature map.
+- Each convolution reduces dimensions and applies ReLU activation.
+- Batch normalization is applied after each convolution.
 """
 function conv_3x3(in_chs::Int, out_chs::Int)
     Chain(
@@ -115,12 +114,11 @@ end
 """
     copy_and_crop(x, bridge)
 
-    Adjusts the size of the 'bridge' feature map (from downsampling path) to
-    match 'x' using slicing and concatenates them along the third dimension,
-    which represents the channels.
+Adjusts the size of the 'bridge' feature map (from downsampling path) to
+match 'x' using slicing and concatenates them along the channel dimension.
 
-    - This step is crucial for merging features from downsampling and
-      upsampling paths.
+- This step is crucial for merging features from downsampling and upsampling
+  paths.
 """
 function copy_and_crop(x, bridge)
     dx = size(bridge, 1) - size(x, 1)
@@ -133,14 +131,15 @@ function copy_and_crop(x, bridge)
         :
     ]
 
+    # Concatenate along the channel dimension (3rd dimension)
     return cat(x, cropped_bridge, dims = 3)
 end
 
 """
     max_pool_2x2()
 
-    Implements a 2x2 max pooling layer with a stride of 2 to reduce the spatial
-    dimensions of the feature map by half.
+Implements a 2x2 max pooling layer with a stride of 2 to reduce the spatial
+dimensions of the feature map by half.
 """
 function max_pool_2x2()
     MaxPool((2, 2), stride = 2)
@@ -149,11 +148,11 @@ end
 """
     up_conv_2x2(in_chs::Int, out_chs::Int)
 
-    Defines a transposed convolutional layer that increases the spatial
-    dimensions of the feature maps.
+Defines a transposed convolutional layer that increases the spatial
+dimensions of the feature maps, followed by batch normalization and ReLU
+activation.
 
-    - Used in upsampling to recover the original dimensions of the image.
-    - Transposed convolution increases the size of the feature map.
+- Used in upsampling to recover the original dimensions of the image.
 """
 function up_conv_2x2(in_chs::Int, out_chs::Int)
     Chain(
@@ -168,12 +167,11 @@ end
 """
     conv_1x1(in_chs::Int, out_chs::Int)
 
-    A 1x1 convolutional layer reduces the number of feature channels to the
-    desired output channels.
+A 1x1 convolutional layer reduces the number of feature channels to the
+desired output channels.
 
-    - Used as the final layer to map features to segmentation classes or
-      predictions.
-    - The 1x1 convolution adjusts the number of channels.
+- Used as the final layer to map features to segmentation classes or
+  predictions.
 """
 function conv_1x1(in_chs::Int, out_chs::Int)
     Chain(
@@ -183,57 +181,71 @@ function conv_1x1(in_chs::Int, out_chs::Int)
     )
 end
 
+# Constructor Functions for Blocks
+
 """
     UNetDownBlock(in_chs::Int, out_chs::Int)
 
-    Constructor function to create a downsampling block with specified channels.
+Creates a downsampling block with specified channels.
+
+- Applies convolutional layers and max pooling.
 """
 function UNetDownBlock(in_chs::Int, out_chs::Int)
     conv = conv_3x3(in_chs, out_chs)
     pool = max_pool_2x2()
 
-    UNetDownBlock(conv, pool)
+    return UNetDownBlock(conv, pool)
 end
 
 """
     UNetBottleneckBlock(in_chs::Int, out_chs::Int)
 
-    Constructor function to create a downsampling block with specified channels.
+Creates the bottleneck block with specified channels.
+
+- Applies convolutional layers without pooling.
 """
 function UNetBottleneckBlock(in_chs::Int, out_chs::Int)
     conv = conv_3x3(in_chs, out_chs)
 
-    UNetBottleneckBlock(conv)
+    return UNetBottleneckBlock(conv)
 end
 
 """
     UNetUpBlock(in_chs::Int, out_chs::Int)
 
-    Constructor function to create an upsampling block with specified channels.
+Creates an upsampling block with specified channels.
+
+- Applies transposed convolution for upsampling and convolutional layers
+  for feature refinement.
 """
 function UNetUpBlock(in_chs::Int, out_chs::Int)
     upconv = up_conv_2x2(in_chs, out_chs)
     conv = conv_3x3(in_chs, out_chs)
 
-    UNetUpBlock(upconv, conv)
+    return UNetUpBlock(upconv, conv)
 end
 
 """
     UNetOutputBlock(in_chs::Int, out_chs::Int)
 
-    Constructor function to create an output block with a 1x1 convolution layer.
+Creates an output block with a 1x1 convolution layer.
+
+- Reduces the number of channels to match the number of labels/classes.
 """
 function UNetOutputBlock(in_chs::Int, out_chs::Int)
     conv = conv_1x1(in_chs, out_chs)
 
-    UNetOutputBlock(conv)
+    return UNetOutputBlock(conv)
 end
 
 """
-    UNet()
+    UNet(channels::Int=1, labels::Int=1)
 
-    Constructor for U-Net which initializes the downsampling, bottleneck,
-    upsampling and output layers.
+Constructor for U-Net which initializes the downsampling, bottleneck,
+upsampling and output layers.
+
+- `channels`: Number of input channels.
+- `labels`: Number of output labels/classes.
 """
 function UNet(channels::Int = 1, labels::Int = 2)
     downsample = Chain(
@@ -258,96 +270,67 @@ function UNet(channels::Int = 1, labels::Int = 2)
         UNetOutputBlock(64, labels)
     )
 
-    UNet(downsample, bottleneck, upsample, output)
+    return UNet(downsample, bottleneck, upsample, output)
 end
 
 """
     (model::UNet)(x::AbstractArray)
 
-    Applies the entire U-Net model to process an input image through various
-    layers to produce a segmented output.
+Applies the entire U-Net model to process an input image through various
+layers to produce a segmented output.
 
-    - Processes the input through the downsampling path.
-    - Passes the downsampled features through the bottleneck.
-    - Processes the bottleneck features through the upsampling path.
-    - Produces the final output through the output layer.
+- Processes the input through the downsampling path.
+- Passes the downsampled features through the bottleneck.
+- Processes the bottleneck features through the upsampling path.
+- Produces the final output through the output layer.
 """
-function (model::UNet)(x::AbstractArray)
-    println("Input size: ", size(x))
-    
+function (model::UNet)(x::AbstractArray)    
     # Downsampling path
     x1 = model.downsample.layers[1].conv(x)
-    println("\nAfter downsample layer 1 (conv): ", size(x1))
-
     x1_pool = model.downsample.layers[1].pool(x1)
-    println("After downsample layer 1 (max pool): ", size(x1_pool))
 
     x2 = model.downsample.layers[2].conv(x1_pool)
-    println("\nAfter downsample layer 2 (conv): ", size(x2))
-
     x2_pool = model.downsample.layers[2].pool(x2)
-    println("After downsample layer 2 (max pool): ", size(x2_pool))
 
     x3 = model.downsample.layers[3].conv(x2_pool)
-    println("\nAfter downsample layer 3 (conv): ", size(x3))
-
     x3_pool = model.downsample.layers[3].pool(x3)
-    println("After downsample layer 3 (max pool): ", size(x3_pool))
 
     x4 = model.downsample.layers[4].conv(x3_pool)
-    println("\nAfter downsample layer 4 (conv): ", size(x4))
-
     x4_pool = model.downsample.layers[4].pool(x4)
-    println("After downsample layer 4 (max pool): ", size(x4_pool))
 
     # Bottleneck
     x_bottleneck = model.bottleneck.layers[1].conv(x4_pool)
-    println("\nAfter bottleneck: ", size(x_bottleneck))
 
     # Upsampling path
     x_up1_conv = model.upsample.layers[1].upconv(x_bottleneck)
-    println("\nAfter upsample layer 1 (upconv): ", size(x_up1_conv))
-
     x_up1 = model.upsample.layers[1].conv(copy_and_crop(x_up1_conv, x4))
-    println("After upsample layer 1 (conv): ", size(x_up1))
 
     x_up2_conv = model.upsample.layers[2].upconv(x_up1)
-    println("\nAfter upsample layer 2 (upconv): ", size(x_up2_conv))
-
     x_up2 = model.upsample.layers[2].conv(copy_and_crop(x_up2_conv, x3))
-    println("After upsample layer 2 (conv): ", size(x_up2))
 
     x_up3_conv = model.upsample.layers[3].upconv(x_up2)
-    println("\nAfter upsample layer 3 (upconv): ", size(x_up3_conv))
-
     x_up3 = model.upsample.layers[3].conv(copy_and_crop(x_up3_conv, x2))
-    println("After upsample layer 3 (conv): ", size(x_up3))
 
     x_up4_conv = model.upsample.layers[4].upconv(x_up3)
-    println("\nAfter upsample layer 4 (upconv): ", size(x_up4_conv))
-
     x_up4 = model.upsample.layers[4].conv(copy_and_crop(x_up4_conv, x1))
-    println("After upsample layer 4 (conv): ", size(x_up4))
 
     # Output layer
     output = model.output[1].conv(x_up4)
-    println("\nOutput size: ", size(output))
 
-    println()
-    output
+    return output
 end
 
 """
     Base.show(io::IO, model::UNet)
 
-    Customizes the display of the U-Net model's structure, showing the
-    dimensions of convolutional layers at each stage.
+Customizes the display of the U-Net model's structure, showing the
+dimensions of convolutional layers at each stage.
 
-    - `io`: The I/O stream to print to.
-    - `model`: The U-Net model instance.
+- `io`: The I/O stream to print to.
+- `model`: The U-Net model instance.
 
-    Prints the dimensions of the convolutional layers in the downsampling path,
-    the bottleneck, the upsampling path and the output layer.
+Prints the dimensions of the convolutional layers in the downsampling path,
+the bottleneck, the upsampling path and the output layer.
 """
 function Base.show(io::IO, model::UNet)
     println(io, "UNet Structure:")
